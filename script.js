e1c283fc1a356c6b7fb549a942d35bc25d2fa999
BEAstro <89750913+BEAstro@users.noreply.github.com>
2026-08-30 00:55:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function playMusic() {
+    alert("BEAstro music player coming soon 🎵");
+}
+
+// Smooth navigation
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (event) {
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            event.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
